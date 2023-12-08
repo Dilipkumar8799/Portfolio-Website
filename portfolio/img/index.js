61ade0e1cbb9154e7b9eb4743a7e890d88b0dae5
ImDilipkumar <96405035+Dilipkumar8799@users.noreply.github.com>
2023-12-08 14:45:02 +0530

    Add files via upload

---
+++ portfolio/img/index.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector("#menu-btn")
+let header = document.querySelector(".header")
+let service = document.querySelector(".service")
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+let themeToggler = document.querySelector("#theme-toggler")
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }
+    else{
+        document.body.classList.remove('active')
+    }
+
+}
+
+service.onclick = () =>{
+    service.classList.toggle('active');
+}
+
+
